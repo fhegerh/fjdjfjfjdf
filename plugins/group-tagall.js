942c9d90eee4ce5a4a8140fc5bd15c8bfd339ee9
fhegerh <114147702+fhegerh@users.noreply.github.com>
2026-06-07 13:50:25 +0500

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -48,7 +48,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, isAdmins, isCreator, 
             teks += `${randomEmoji} @${mem.id.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ KAMRAN ┃ XD ✪──";
+        teks += "└──✪ KAMRAN ┃ MD ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: participants.map(a => a.id) }, { quoted: mek });
 
