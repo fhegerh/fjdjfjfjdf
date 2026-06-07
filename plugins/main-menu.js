8dd949be4aaff91efd94f5ca4975affa7d7561d8
fhegerh <114147702+fhegerh@users.noreply.github.com>
2026-06-07 08:47:22 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -7,9 +7,9 @@ const {runtime} = require('../lib/functions')
 const axios = require('axios')
 
 cmd({
-pattern: "menu2",
+pattern: "menu",
 alias: ["allmenu","fullmenu"],
-use: '.menu2',
+use: '.menu',
 desc: "Show all bot commands",
 category: "menu",
 react: "📜",
