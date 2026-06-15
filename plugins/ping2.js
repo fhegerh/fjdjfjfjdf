f7b4373db9d48ed88deecd956b3a23b0593b9ccd
fhegerh <114147702+fhegerh@users.noreply.github.com>
2026-06-15 21:38:05 +0500

    Update ping2.js

--- plugins/ping2.js
+++ plugins/ping2.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363424896372406@newsletter',
+                    newsletterJid: '120363408038738494@newsletter',
                     newsletterName: "DR KAMRAN",
                     serverMessageId: 143
                 }
